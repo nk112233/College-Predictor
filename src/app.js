5992c675191198fbceac9d6e709a212f0d455de8
nk112233 <nikunjkadu19@gmail.com>
2024-06-26 11:14:15 +0530

    dist folder added

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.get("/api" , showTable );
 app.use("/api",clgRouter);
 
-app.use(express.static(path.join(__dirname , '../client/dist')))
+app.use(express.static(path.join(__dirname , '../client/dist/assets')))
 
 app.get('*' , (req , res) => res.sendFile(path.join(__dirname , '../client/dist/index.html')))
 
