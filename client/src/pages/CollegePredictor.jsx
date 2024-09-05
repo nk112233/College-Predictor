fb12a7b08a47c3efddb96365d423b6a1f682f9d3
nk112233 <nikunjkadu19@gmail.com>
2024-09-05 22:49:20 +0530

    Added 2024 Cutoffs

--- client/src/pages/CollegePredictor.jsx
+++ client/src/pages/CollegePredictor.jsx
@@ -70,7 +70,7 @@ const CollegePredictor = () => {
   useEffect(() => {
     const loadingToastId = toast.loading("Loading Site Please Wait...");
     axios
-      .get(`${url}/api?round=1`)
+      .get(`${url}/api?round=4`)
       .then((res) => {
         setCategory(res.data.data.category);
         setBranch(res.data.data.branch);
@@ -747,6 +747,22 @@ const CollegePredictor = () => {
             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Round 3
+          </label>
+        </div>
+        <div className="flex items-center me-4">
+          <input
+            id="inline-3-radio"
+            type="radio"
+            value={4}
+            name="inline-radio-group"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            onClick={() => setRound(4)}
+          />
+          <label
+            htmlFor="inline-3-radio"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            2024 CAP Round 3
           </label>
         </div>
       </div>
